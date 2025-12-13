de282c28700f9e04414ef72017727512630b5b49
Scotte Zinn <scotte@zinn.ca>
2025-12-13 16:21:31 -0500

    Change setup to show branches (#10)
    Change setup to show the test branches and commit order

--- setup.fish
+++ setup.fish
@@ -1,16 +1,16 @@
 #!/opt/homebrew/bin/fish
 
 ls >>first
-jj commit -m "First change"
+jj commit -m "1-1. First change"
 ls >>second
-jj commit -m "Second change"
+jj commit -m "1-2. Second change"
 ls >>third
-jj commit -m "Third change"
+jj commit -m "1-3. Third change"
 jj new green
 ls >>branch-1
-jj commit -m "Another branch"
+jj commit -m "2-1. Another branch"
 ls >>branch-1
-jj commit -m "More on branch"
+jj commit -m "2-2. More on branch"
 jj new green
 ls >>branch-2
-jj commit -m "Branch 2"
+jj commit -m "3-1. Branch 2"

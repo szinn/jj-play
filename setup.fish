d0056f54c4f9232698cde70b6cd2ac0109430ccc
Scotte Zinn <scotte@zinn.ca>
2025-12-13 17:13:59 -0500

    Refine setup
    Refine script to avoid collisions

--- setup.fish
+++ setup.fish
@@ -2,15 +2,15 @@
 
 ls >>first
 jj commit -m "1-1. First change"
-ls >>second
+ls >>first
 jj commit -m "1-2. Second change"
-ls >>third
+ls >>first
 jj commit -m "1-3. Third change"
 jj new green
-ls >>branch-1
+ls >>second
 jj commit -m "2-1. Another branch"
-ls >>branch-1
+ls >>second
 jj commit -m "2-2. More on branch"
 jj new green
-ls >>branch-2
+ls >>third
 jj commit -m "3-1. Branch 2"

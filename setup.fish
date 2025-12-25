041439749e27662491fca27f922726289bd5c621
Scotte Zinn <scotte@zinn.ca>
2025-12-25 16:51:42 -0500

    Updated setup script (#55)
    again

--- setup.fish
+++ setup.fish
@@ -2,14 +2,17 @@
 
 ls >>first
 jj commit -m "1-1. First change\n\nFirst"
+jj new
 ls >>first
 jj commit -m "1-2. Second change\n\nSecond"
+jj new
 ls >>first
 jj commit -m "1-3. Third change\n\nThird"
 
 jj new green
 ls >>second
 jj commit -m "2-1. First change\n\nFourth"
+jj new
 ls >>second
 jj commit -m "2-2. Second change\n\nFifth"
 

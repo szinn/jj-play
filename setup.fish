97aa7ebdb53588377f707ec7b9787fe1e4250858
Scotte Zinn <scotte@zinn.ca>
2025-12-25 20:33:04 -0500

    Update setup
    Allow multiline comments

--- setup.fish
+++ setup.fish
@@ -1,21 +1,21 @@
 #!/opt/homebrew/bin/fish
 
 ls >>first
-jj commit -m "1-1. First change\n\nFirst"
+jj commit -m "1-1. First change" -m First
 jj new
 ls >>first
-jj commit -m "1-2. Second change\n\nSecond"
+jj commit -m "1-2. Second change" -m Second
 jj new
 ls >>first
-jj commit -m "1-3. Third change\n\nThird"
+jj commit -m "1-3. Third change" -m Third
 
 jj new green
 ls >>second
-jj commit -m "2-1. First change\n\nFourth"
+jj commit -m "2-1. First change" -m Fourth
 jj new
 ls >>second
-jj commit -m "2-2. Second change\n\nFifth"
+jj commit -m "2-2. Second change" -m Fifth
 
 jj new green
 ls >>third
-jj commit -m "3-1. First change\n\nSix"
+jj commit -m "3-1. First change" -m ix
